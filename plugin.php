31b11565aea8fe7d2cb8f0c1acd55f87d662a0cb
Scott Baggett <scott@badassembly.com>
2014-08-31 18:47:29 -0700

    1.01

--- plugin.php
+++ plugin.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: ACF + WP-JSON
 Description: Embeds ACF fields in post-meta. That's it.
-Version: 1.0
+Version: 1.01
 Author: scott baggett
+AuthorURI: http://www.github.com/scottbaggett
 */
-
 
 add_action( 'plugins_loaded', 'add_json_filter' );
 
@@ -15,10 +15,9 @@ function add_json_filter()
     add_filter( 'json_prepare_post', 'apply_json_filter', 10, 3 );
 }
 
+# add in all ACF meta properties
 function apply_json_filter( $_post, $post, $context )
 {
-     # add in all ACF meta properties
     $_post['meta'] = array_merge($_post['meta'],get_fields($post['ID']));
     return $_post;
-
 }
